d24a384b5e5d02f0f3076b4db55a0d94498b0a65
m-strw <69999281+m-strw@users.noreply.github.com>
2020-12-15 09:27:39 +1300

    inital setup

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import landingPage from './landingPage'
+
+const App = () => {
+  return (
+  <>
+  {landingPage}
+  </>
+
+    )
+}
+
+export default App
---
+++ client/components/landingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const landingPage = () => {
+  return (
+    <h1>React development has begun!</h1>
+  )
+}
+
+export default landingPage
