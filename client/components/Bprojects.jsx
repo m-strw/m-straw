32842a83698117495cd98fb1ea95bd80ba2a5a3d
m-strw <69999281+m-strw@users.noreply.github.com>
2020-12-15 22:44:32 +1300

    Basic card-layout

--- client/components/Bprojects.jsx
+++ client/components/Bprojects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Bprojects = () => {
   return (
-  
+  <div>
     <div className ='bpro' >
     <h2>My Big Projects</h2>
     <p>
@@ -10,7 +10,22 @@ const Bprojects = () => {
     It was great fun being able to students with there learning but after doing this I knew I needed techincal skill to move forward.
     </p>
     </div>
-  
+    
+    <div className= 'larger-Card'>
+    
+    <div className= 'Card-Heading-Info'>
+    <p className= 'pTag-Title' >Building Peoply</p>
+    </div>
+   <div className= 'Card-info'>
+    <p className = 'pTag-body'>
+      An application that allows users to view a poem at random from a .json file, this was before I knew react native and data bases so I used handlebars. 
+
+      worked on this with two other students from my cohort and we pair programme this project.  
+    </p>
+   </div>
+    </div>
+
+    </div>  
     )
 }
 
