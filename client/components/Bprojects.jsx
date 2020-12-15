f6c7555f2e7c8bcddc591609a58cac4591c531f4
m-strw <69999281+m-strw@users.noreply.github.com>
2020-12-15 16:03:18 +1300

    add messege

---
+++ client/components/Bprojects.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Bprojects = () => {
+  return (
+  
+    <div className ='bpro' >
+    <h2>My Big Projects</h2>
+    <p>
+    I didn’t take a techincal roll, I was the founder in these projects. 
+    It was great fun being able to students with there learning but after doing this I knew I needed techincal skill to move forward.
+    </p>
+    </div>
+  
+    )
+}
+
+export default Bprojects
--- client/components/landingPage.jsx
+++ client/components/landingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Bprojects from './Bprojects'
 
 
 
@@ -7,11 +8,11 @@ const landingPage = () => {
     <div className ='heading-Info' >
     <h1>Hey, I’m Matt Strawbridge</h1>
 
-    <p>I’m a Full Stack Web-Devloper and  love working on things that make peoples lifes easier and better.</p>
+    <p>I’m a <em className= 'red'>Full Stack Web-Devloper</em> and  love working on things that make peoples lifes easier and better.</p>
 
-    <h2>My Code and Info.</h2>
-    <div>
-    <div className ='Links'>
+    <h2 className='margin-top' >My Code and Info.</h2>
+    <div className='links-Info'>
+    <div className='Links'>
     <ul>
       <li className='li' >
       <a href="strawbridgeml@gmail.com">Github</a>
@@ -33,9 +34,11 @@ const landingPage = () => {
     </ul>
     </div>
 
+    {Bprojects}
+    </div>
     </div>
 
-    </div>
+
 
   )
 }
